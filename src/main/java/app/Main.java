399e007124299eec685ffd703e6893983ddfb6e6
Mohammed Abdelbarry <m7md337@gmail.com>
2018-05-15 13:15:51 +0200

    Added latest version of javafx view files

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,24 @@
 package app;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/signIn.fxml"));
+        primaryStage.setTitle("Library Bookstores!");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
