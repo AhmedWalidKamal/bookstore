5bb6fe943a2b37a76897b0df68e4e2b332d1418f
Ahmed Walid <ahmed3walid96@gmail.com>
2018-05-15 04:56:32 +0200

    Added basic navigation between sign in/up pages

--- src/controller/SignIn.java
+++ src/controller/SignIn.java
@@ -1,4 +1,48 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class SignIn {
+
+    @FXML
+    private JFXButton signUpButton;
+
+    @FXML
+    private JFXButton signInButton;
+
+
+    @FXML
+    private void handleSignUpButtonAction(ActionEvent event) {
+        closeStage();
+        loadMain();
+    }
+
+    @FXML
+    private void handleSignInButtonAction(ActionEvent event) {
+    }
+
+    private void closeStage() {
+        ((Stage) signUpButton.getScene().getWindow()).close();
+    }
+
+    private void loadMain() {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/view/signUp.fxml"));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle("Register");
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException ex) {
+            // Log Exception
+        }
+    }
 }
