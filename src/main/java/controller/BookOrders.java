b7a2fe07f979d6fe939307fe5dca7e26a38c964a
Ahmed Walid <ahmed3walid96@gmail.com>
2018-06-09 13:58:42 +0200

    Added orders tab UI and seperated adminstration controllers and fxmls

---
+++ src/main/java/controller/BookOrders.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class BookOrders {
+
+    @FXML
+    private AnchorPane ordersRootPane;
+
+    @FXML
+    public void initialize() {
+
+    }
+}
