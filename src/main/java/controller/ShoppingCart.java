84151066256056373bcd1f02da583a9848c4e5e3
MohamedAshrafTolba <tolba96mohamed@gmail.com>
2018-06-11 05:52:50 +0200

    Added purchase dialog controller.

--- src/main/java/controller/ShoppingCart.java
+++ src/main/java/controller/ShoppingCart.java
@@ -1,15 +1,37 @@
 package controller;
 
+import com.gluonhq.charm.glisten.control.CardPane;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
 class ShoppingCart {
 
+    @FXML
+    private Label numberOfItems;
+
+    @FXML
+    private Label totalPriceLabel;
+
+    @FXML
+    private JFXButton purchaseButton;
+
+    @FXML
+    private CardPane cartItemCardPane;
+
+    private StackPane rootPane;
+
+    private PurchaseDialog purchaseDialog;
+
     private Node node;
 
-    ShoppingCart() {
+    ShoppingCart(StackPane rootPane) {
+        this.rootPane = rootPane;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/shoppingCart.fxml"));
         fxmlLoader.setController(this);
         try {
@@ -17,9 +39,21 @@ class ShoppingCart {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        init();
     }
 
     Node getNode() {
         return node;
+    }
+
+    private void init() {
+        purchaseButton.setOnMouseClicked(mouseEvent -> handlePurchaseButtonAction());
+    }
+
+    private void handlePurchaseButtonAction() {
+        if (purchaseDialog == null) {
+            purchaseDialog = new PurchaseDialog(rootPane);
+        }
+        purchaseDialog.show();
     }
 }
