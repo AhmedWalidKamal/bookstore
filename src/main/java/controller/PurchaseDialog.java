11a905e95f3f3ed7fea9f50dfa90a1de29f9ddb4
MohamedAshrafTolba <tolba96mohamed@gmail.com>
2018-06-12 03:27:46 +0200

    Validated purchase dialog field.

--- src/main/java/controller/PurchaseDialog.java
+++ src/main/java/controller/PurchaseDialog.java
@@ -13,6 +13,8 @@ import javafx.scene.layout.StackPane;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class PurchaseDialog {
 
@@ -37,6 +39,13 @@ class PurchaseDialog {
 
     private ShoppingCart parentController;
 
+    private static final Pattern VALID_CCN_REGEX =
+            Pattern.compile("^(4[0-9]{12}(?:[0-9]{3})?)"
+                    + "|((?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12})$");
+
+    private static final Pattern VALID_EXPIRE_DATE =
+            Pattern.compile("^(0[1-9]|10|11|12)/20[0-9]{2}$");
+
     PurchaseDialog(StackPane rootPane, ShoppingCart parent) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/purchaseDialog.fxml"));
         fxmlLoader.setController(this);
@@ -55,6 +64,10 @@ class PurchaseDialog {
     }
 
     void show() {
+        creditCardNumberField.resetValidation();
+        expirationDateField.resetValidation();
+        shippingAddressField.resetValidation();
+        clearInputFields();
         dialog.show();
     }
 
@@ -71,18 +84,19 @@ class PurchaseDialog {
         LinkedHashMap<String, Integer> booksPurchased
                 = MainController.getInstance().getCurrentUser().getCart().getCart();
         try {
-            if (MainController.getInstance().getBackendService().buyBooks(
-                    MainController.getInstance().getCurrentUser().getUserID(),
-                    booksPurchased)) {
-                parentController.clear();
-                parentController.showMessage("Purchased Successfully");
-            } else {
-                parentController.showError("Failed to buy books");
+            if (validateFields()) {
+                if (MainController.getInstance().getBackendService().buyBooks(
+                        MainController.getInstance().getCurrentUser().getUserID(),
+                        booksPurchased)) {
+                    parentController.clear();
+                    parentController.showMessage("Purchased Successfully");
+                } else {
+                    parentController.showError("Failed to buy books");
+                }
             }
         } catch (SQLException e) {
             parentController.showError("Failed to buy books");
         }
-
         close();
     }
 
@@ -121,10 +135,37 @@ class PurchaseDialog {
         dialog.close();
     }
 
-    // To be used on clearing the user session data.
     private void clearInputFields() {
         creditCardNumberField.clear();
         expirationDateField.clear();
         shippingAddressField.clear();
+    }
+
+    private boolean validateFields() {
+        if (!validateCCN()) {
+            parentController.showError("Invalid Credit Card Number!");
+            return false;
+        } else if (!validateExpirationDate()) {
+            parentController.showError("Invalid Expiration Date!");
+            return false;
+        } else if (!validateShippingAddress()) {
+            parentController.showError("Invalid Shipping Address!");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateCCN() {
+        Matcher matcher = VALID_CCN_REGEX.matcher(creditCardNumberField.getText());
+        return matcher.find();
+    }
+
+    private boolean validateExpirationDate() {
+        Matcher matcher = VALID_EXPIRE_DATE.matcher(expirationDateField.getText());
+        return matcher.find();
+    }
+
+    private boolean validateShippingAddress() {
+        return !shippingAddressField.getText().isEmpty();
     }
 }
